a8874bf7390410921ba9a2222dd8130430c4b6d0
narendra4912 <narendra4912@gmail.com>
2021-12-15 17:18:05 +0530

    UC3 - UC8 tested and passed

--- src/main/java/UserValidation.java
+++ src/main/java/UserValidation.java
@@ -20,7 +20,7 @@ public class UserValidation {
             return false;
     }
 
-    public void validUserLastName(String lastName)
+    public boolean validUserLastName(String lastName)
     {
         this.lastName = lastName;
         String userRegex = "^[A-Z]{1}[A-Za-z0-9]{2}$";
@@ -28,12 +28,14 @@ public class UserValidation {
         Matcher matcher = pattern.matcher(lastName);
 
         if(matcher.matches())
-            System.out.println("User's Last Name = "+ lastName +" is matching with the pattern !!!");
+            return true;
+            //System.out.println("User's Last Name = "+ lastName +" is matching with the pattern !!!");
         else
-            System.out.println("User's Last Name = "+ lastName +" is not matching with the pattern !!!");
+            return false;
+        //System.out.println("User's Last Name = "+ lastName +" is not matching with the pattern !!!");
     }
 
-    public void validEmailAddress(String email)
+    public boolean validEmailAddress(String email)
     {
         this.email = email;
         String regex = "^[a-z0-9]{3,20}([+._-][a-z0-9]+)?@[a-z0-9]+.[a-z0-9]{2,3}(.[a-z]{2})?$";
@@ -41,12 +43,14 @@ public class UserValidation {
         Matcher matcher = pattern.matcher(email);
 
         if(matcher.matches())
-            System.out.println("User's Email Address = "+ email +" is matching with the pattern !!!");
+            return true;
+            //System.out.println("User's Email Address = "+ email +" is matching with the pattern !!!");
         else
-            System.out.println("User's Email Address = "+ email +" is not matching with the pattern !!!");
+            return false;
+            //System.out.println("User's Email Address = "+ email +" is not matching with the pattern !!!");
     }
 
-    public void validMobileNo(String mobile)
+    public boolean validMobileNo(String mobile)
     {
         this.mobileNo = mobile;
         String regex = "^[1-9]{2}( )[0-9]{10}$";
@@ -54,12 +58,14 @@ public class UserValidation {
         Matcher matcher = pattern.matcher(String.valueOf(mobileNo));
 
         if(matcher.matches())
-            System.out.println("User's Mobile No = "+ mobileNo +" is matching with the pattern !!!");
+            return true;
+            //System.out.println("User's Mobile No = "+ mobileNo +" is matching with the pattern !!!");
         else
-            System.out.println("User's Mobile No = "+ mobileNo +" is not matching with the pattern !!!");
+            return false;
+            //System.out.println("User's Mobile No = "+ mobileNo +" is not matching with the pattern !!!");
     }
 
-    public void validPassword(String password) {
+    public boolean validPassword(String password) {
 
         this.password = password;
         String regex="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,12}";
@@ -67,8 +73,10 @@ public class UserValidation {
         Matcher matcher = pattern.matcher(password);
 
         if(matcher.matches())
-            System.out.println("User's Password = "+ password +" is accepted !!!");
+            return true;
+            //System.out.println("User's Password = "+ password +" is accepted !!!");
         else
-            System.out.println("User's Password = "+ password +" is not following the standard password rules !!!");
+            return false;
+            //System.out.println("User's Password = "+ password +" is not following the standard password rules !!!");
     }
 }
--- src/test/java/UserFieldDataTesting.java
+++ src/test/java/UserFieldDataTesting.java
@@ -1,8 +1,11 @@
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserFieldDataTesting {
     /*String firstName, lastName, email;
@@ -12,8 +15,13 @@ public class UserFieldDataTesting {
     Scanner sc = new Scanner(System.in);*/
 
     @Before
-    public void setUpEnvironment() {
+    public void setUpBeforeEnvironment() {
         System.out.println("Inside the @Before Method");
+    }
+
+    @After
+    public void setUpAfterEnvironment() {
+        System.out.println("Inside the @After Method");
     }
 
     @Test
@@ -27,14 +35,97 @@ public class UserFieldDataTesting {
     }
 
     @Test
-    public void givenLastNameShouldPassWhenFirstNameRulesSatisfied() {
+    public void givenLastNameShouldPassWhenLastNameRulesSatisfied() {
 
         String lastName = "Xyz";
         UserValidation user = new UserValidation();
-        boolean actualResult = user.validUserFirstName(lastName);
+        boolean actualResult = user.validUserLastName(lastName);
         boolean expectedResult = true;
         Assert.assertEquals(expectedResult, actualResult);
     }
 
 
+    @Test
+    public void givenEmailShouldPassWhenEmailRulesSatisfied() {
+
+        //Happy Email testing
+        String email = "abc.xyz@domain.co.in";
+        UserValidation user = new UserValidation();
+        boolean actualResult = user.validEmailAddress(email);
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenEmailShouldPassWhenEmailRulesSatisfied2() {
+
+        //Sad Email testing
+        String email = "abc.xyz@domain.co.india";
+        UserValidation user = new UserValidation();
+        boolean actualResult = user.validEmailAddress(email);
+        boolean expectedResult = true;
+        Assert.assertNotEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenMobileNoShouldPassWhenMobileNoRulesSatisfied() {
+
+        String mobileNo = "91 9876543210";
+        UserValidation user = new UserValidation();
+        boolean actualResult = user.validMobileNo(mobileNo);
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenPasswordShouldPassWhenPasswordIs8DigitInLength() {
+
+        String password = "abcdefgh";
+        String regex=".{8,12}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        boolean actualResult = matcher.matches();
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenPasswordShouldPassWhenPasswordHasMinimum1CapitalLetter() {
+
+        String password = "Abcdefgh";
+        String regex="(?=.*[A-Z]).{8,12}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        boolean actualResult = matcher.matches();
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenPasswordShouldPassWhenPasswordHasMinimum1NumericLetter() {
+
+        String password = "Abcde1gh";
+        String regex="(?=.*[0-9]).{8,12}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        boolean actualResult = matcher.matches();
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenPasswordShouldPassWhenPasswordHasExact1SpecialLetter() {
+
+        String password = "Abcd@1gh";
+        String regex="(?=.*[!@#$%^&*]).{8,12}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+
+        boolean actualResult = matcher.matches();
+        boolean expectedResult = true;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
 }
