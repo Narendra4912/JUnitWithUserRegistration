62a8ee0975d7fb6ddfdd4010555cbd08ea27bf1b
narendra4912 <narendra4912@gmail.com>
2021-12-15 11:28:20 +0530

    New project for JUnit with user registration is created

---
+++ src/main/java/UserRegistration.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+
+        String firstName, lastName, email;
+        String mobileNo, password;
+
+        UserValidation user = new UserValidation();
+        Scanner sc = new Scanner(System.in);
+
+            /*System.out.println("Enter User's First Name = ");
+            firstName = sc.next();
+            user.validUserFirstName(firstName);
+
+            System.out.println("Enter User's Last Name = ");
+            lastName = sc.next();
+            user.validUserLastName(lastName);
+
+            System.out.println("Enter User's Email Address  = ");
+            email = sc.next();
+            user.validEmailAddress(email);
+
+            System.out.println("Enter User's Mobile No = ");
+            mobileNo = sc.nextLine();
+            user.validMobileNo(mobileNo);*/
+
+        System.out.println("Enter User's Password = ");
+        password = sc.nextLine();
+        user.validPassword(password);
+    }
+}
